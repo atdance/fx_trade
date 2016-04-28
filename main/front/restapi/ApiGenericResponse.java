faa140721c8356d853875660b81014eb68d1e0a6
atdance <miccia2@mail.com>
2016-04-28 12:38:39 +0200

    remeoved imports of apache.commons.lang.builder.EqualsBuilder HashCodeBuilder ToStringBuilder

--- main/front/restapi/ApiGenericResponse.java
+++ main/front/restapi/ApiGenericResponse.java
@@ -5,10 +5,6 @@ import java.util.Map;
 
 import javax.annotation.Generated;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
-
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -19,8 +15,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 /**
  * ApiGenericResponse
  * <p>
- * 
- * 
+ *
+ *
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
@@ -29,19 +25,19 @@ public class ApiGenericResponse {
 
 	/**
 	 * Response status (Required)
-	 * 
+	 *
 	 */
 	@JsonProperty("status")
 	private String status;
 	/**
 	 * Response message
-	 * 
+	 *
 	 */
 	@JsonProperty("message")
 	private String message;
 	/**
 	 * User id (paid)
-	 * 
+	 *
 	 */
 	@JsonProperty("user")
 	private Long user;
@@ -50,7 +46,7 @@ public class ApiGenericResponse {
 
 	/**
 	 * Response status (Required)
-	 * 
+	 *
 	 * @return The status
 	 */
 	@JsonProperty("status")
@@ -60,7 +56,7 @@ public class ApiGenericResponse {
 
 	/**
 	 * Response status (Required)
-	 * 
+	 *
 	 * @param status
 	 *            The status
 	 */
@@ -71,7 +67,7 @@ public class ApiGenericResponse {
 
 	/**
 	 * Response message
-	 * 
+	 *
 	 * @return The message
 	 */
 	@JsonProperty("message")
@@ -81,28 +77,13 @@ public class ApiGenericResponse {
 
 	/**
 	 * Response message
-	 * 
+	 *
 	 * @param message
 	 *            The message
 	 */
 	@JsonProperty("message")
 	public void setMessage(String message) {
 		this.message = message;
-	}
-
-	@Override
-	public String toString() {
-		return ToStringBuilder.reflectionToString(this);
-	}
-
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
-
-	@Override
-	public boolean equals(Object other) {
-		return EqualsBuilder.reflectionEquals(this, other);
 	}
 
 	@JsonAnyGetter
@@ -113,6 +94,85 @@ public class ApiGenericResponse {
 	@JsonAnySetter
 	public void setAdditionalProperty(String name, Object value) {
 		this.additionalProperties.put(name, value);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "ApiGenericResponse [status=" + status + ", message=" + message
+				+ ", user=" + user + ", additionalProperties="
+				+ additionalProperties + "]";
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime
+				* result
+				+ ((additionalProperties == null) ? 0 : additionalProperties
+						.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ApiGenericResponse other = (ApiGenericResponse) obj;
+		if (additionalProperties == null) {
+			if (other.additionalProperties != null) {
+				return false;
+			}
+		} else if (!additionalProperties.equals(other.additionalProperties)) {
+			return false;
+		}
+		if (message == null) {
+			if (other.message != null) {
+				return false;
+			}
+		} else if (!message.equals(other.message)) {
+			return false;
+		}
+		if (status == null) {
+			if (other.status != null) {
+				return false;
+			}
+		} else if (!status.equals(other.status)) {
+			return false;
+		}
+		if (user == null) {
+			if (other.user != null) {
+				return false;
+			}
+		} else if (!user.equals(other.user)) {
+			return false;
+		}
+		return true;
 	}
 
 }
