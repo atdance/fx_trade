7b4bf6fc0c6b99730e9c28745816df01c7bf0779
atdance <miccia2@mail.com>
2016-04-28 12:40:24 +0200

    removed imports of apache.common.build EqualsBuilder HashCodeBuilder ToStringBuilder

--- main/front/restapi/JSONTradeMessage.java
+++ main/front/restapi/JSONTradeMessage.java
@@ -5,9 +5,6 @@ package front.restapi;
 
 import java.math.BigDecimal;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-
 import common.MyMath;
 
 /**
@@ -44,14 +41,108 @@ public class JSONTradeMessage {
 	public JSONTradeMessage() {
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((amountBuy == null) ? 0 : amountBuy.hashCode());
+		result = prime * result
+				+ ((amountSell == null) ? 0 : amountSell.hashCode());
+		result = prime * result
+				+ ((currencyFrom == null) ? 0 : currencyFrom.hashCode());
+		result = prime * result
+				+ ((currencyTo == null) ? 0 : currencyTo.hashCode());
+		result = prime
+				* result
+				+ ((originatingCountry == null) ? 0 : originatingCountry
+						.hashCode());
+		result = prime * result + ((rate == null) ? 0 : rate.hashCode());
+		result = prime * result
+				+ ((timePlaced == null) ? 0 : timePlaced.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
-	public boolean equals(final Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		JSONTradeMessage other = (JSONTradeMessage) obj;
+		if (amountBuy == null) {
+			if (other.amountBuy != null) {
+				return false;
+			}
+		} else if (!amountBuy.equals(other.amountBuy)) {
+			return false;
+		}
+		if (amountSell == null) {
+			if (other.amountSell != null) {
+				return false;
+			}
+		} else if (!amountSell.equals(other.amountSell)) {
+			return false;
+		}
+		if (currencyFrom == null) {
+			if (other.currencyFrom != null) {
+				return false;
+			}
+		} else if (!currencyFrom.equals(other.currencyFrom)) {
+			return false;
+		}
+		if (currencyTo == null) {
+			if (other.currencyTo != null) {
+				return false;
+			}
+		} else if (!currencyTo.equals(other.currencyTo)) {
+			return false;
+		}
+		if (originatingCountry == null) {
+			if (other.originatingCountry != null) {
+				return false;
+			}
+		} else if (!originatingCountry.equals(other.originatingCountry)) {
+			return false;
+		}
+		if (rate == null) {
+			if (other.rate != null) {
+				return false;
+			}
+		} else if (!rate.equals(other.rate)) {
+			return false;
+		}
+		if (timePlaced == null) {
+			if (other.timePlaced != null) {
+				return false;
+			}
+		} else if (!timePlaced.equals(other.timePlaced)) {
+			return false;
+		}
+		if (userId == null) {
+			if (other.userId != null) {
+				return false;
+			}
+		} else if (!userId.equals(other.userId)) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
