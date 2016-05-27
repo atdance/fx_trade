d5d8a844f60f7a92149cd747bb745f9fff1e3f35
atdance <miccia2@mail.com>
2016-05-27 08:17:48 +0200

    jax-rs support

---
+++ src/main/java/com/bondsbiz/trade/business/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.bondsbiz.trade.business;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+
+}
