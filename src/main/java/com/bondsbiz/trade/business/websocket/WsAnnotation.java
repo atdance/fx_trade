8e6139376600c7fd8ba8b4fb137aedacc9e30fb9
atdance <miccia2@mail.com>
2016-05-27 08:12:41 +0200

    @Inject CurrencyMarket

--- src/main/java/com/bondsbiz/trade/business/websocket/WsAnnotation.java
+++ src/main/java/com/bondsbiz/trade/business/websocket/WsAnnotation.java
@@ -1,9 +1,10 @@
-package front.websocket;
+package com.bondsbiz.trade.business.websocket;
 
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.inject.Inject;
 import javax.websocket.EncodeException;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
@@ -11,29 +12,38 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import org.glassfish.tyrus.core.TyrusSession;
 import org.slf4j.LoggerFactory;
 
-import storage.CurrencyMarket;
+import com.bondsbiz.trade.business.storage.CurrencyMarket;
 
-import common.Volume;
+import com.bondsbiz.trade.MyTime;
+import com.bondsbiz.trade.business.model.MarketVolume;
 
-@ServerEndpoint(value = "/websocket/WsAnnotation", encoders = { front.websocket.MyEncoder.class })
+import com.bondsbiz.trade.business.api.TradeAPI;
+import com.bondsbiz.trade.business.encoders.*;
+
+//import org.glassfish.tyrus.core.TyrusSession;
+
+@ServerEndpoint(value = "/websocket/WsAnnotation", encoders = { JsonEncoder.class })
 public class WsAnnotation {
 
-	Volume currencyVolume = null;
+	MarketVolume currencyVolume = null;
 
-	CurrencyMarket mkt = storage.CurrencyMarket.getInstance();
+	@Inject
+	static CurrencyMarket mkt;
 
 	private static Set<Session> sessions = new HashSet<>();
 
-	static org.slf4j.Logger aLOG = null;
+	static org.slf4j.Logger LOG = null;
 
 	static {
-		aLOG = LoggerFactory.getLogger(WsAnnotation.class);
+		LOG = LoggerFactory.getLogger(TradeAPI.class);
 	}
 
 	@OnOpen
 	public void onOpen(Session session) {
+		MyTime timer = new MyTime();
 		sessions.add(session);
 
 		currencyVolume = mkt.volume();
@@ -42,15 +52,16 @@ public class WsAnnotation {
 				session.getBasicRemote().sendObject(currencyVolume);
 			}
 		} catch (IOException ex) {
-			aLOG.error(ex.getMessage(), ex);
+			LOG.error(ex.getMessage(), ex);
 			try {
 				session.close();
 			} catch (IOException ex1) {
-				aLOG.error(ex1.getMessage(), ex1);
+				LOG.error(ex1.getMessage(), ex1);
 			}
 		} catch (EncodeException ex) {
-			aLOG.error(ex.getMessage(), ex);
+			LOG.error(ex.getMessage(), ex);
 		}
+		LOG.info("elapsed " + timer.toString());
 	}
 
 	@OnClose
@@ -59,48 +70,53 @@ public class WsAnnotation {
 	}
 
 	@OnMessage
-	public void echoTextMessage(Session session) {
+	public void echoTextMessage(Session session, boolean last) {
 		currencyVolume = mkt.volume();
+		MyTime timer = new MyTime();
 		try {
 			if (session.isOpen()) {
 				session.getBasicRemote().sendObject(currencyVolume);
 			}
 		} catch (IOException ex) {
-			aLOG.error(ex.getMessage(), ex);
+			LOG.error(ex.getMessage(), ex);
 			try {
 				session.close();
 			} catch (IOException e1) {
-				aLOG.error(e1.getMessage(), e1);
+				LOG.error(e1.getMessage(), e1);
 			}
 		} catch (EncodeException e) {
-			aLOG.error(e.getMessage(), e);
+			LOG.error(e.getMessage(), e);
 		}
+		LOG.info("elapsed " + timer.toString());
 	}
 
 	public static void sendAll() {
+		MyTime timer = new MyTime();
 		synchronized (sessions) {
-			Volume volume = storage.CurrencyMarket.getInstance().volume();
+			MarketVolume volume = mkt.volume();
 			for (Session session : sessions) {
 				if (session.isOpen()) {
 					session.getAsyncRemote().sendObject(volume);
 				}
 			}
 		}
+		LOG.info("elapsed " + timer.toString());
 	}
 
+	// @OnMessage
 	@SuppressWarnings("unused")
 	public static void sendAll2() {
-		@SuppressWarnings("unused")
-		Volume currencyVolume = storage.CurrencyMarket.getInstance().volume();
+		MarketVolume currencyVolume = mkt.volume();
 
+		MyTime timer = new MyTime();
 		int max = 1;
 		int counter = 0;
 		for (Session session : sessions) {
 			if ((counter < max) && session.isOpen()) {
-				// ((TyrusSession)
-				// session).broadcast(currencyVolume.toString());
+				((TyrusSession) session).broadcast(currencyVolume.toString());
 				counter++;
 			}
 		}
+		LOG.info("elapsed " + timer.toString());
 	}
 }
